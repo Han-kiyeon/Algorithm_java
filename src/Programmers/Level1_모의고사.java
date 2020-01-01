ad36f8bb4a8ccf2ec4faeae3f6286ee641aae267
Han-kiyeon <kylin3100@naver.com>
2020-01-02 02:46:42 +0900

    200514 [Programmers] 모의고사

--- src/Programmers/Level1_모의고사.java
+++ src/Programmers/Level1_모의고사.java
@@ -13,6 +13,7 @@ public class Level1_모의고사 {
 		int[] p1 = { 1, 2, 3, 4, 5 };
 		int[] p2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
 		int[] p3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+		
 		int[] cnt = new int[3];
 		for (int i = 0; i < answers.length; i++) {
 			int now = answers[i];
@@ -23,12 +24,14 @@ public class Level1_모의고사 {
 			if (p3[i % p3.length] == now)
 				cnt[2]++;
 		}
+		
 		int max = Math.max(Math.max(cnt[0], cnt[1]), cnt[2]);
 		ArrayList<Integer> list = new ArrayList<>();
 		for (int i = 0; i < cnt.length; i++) {
 			if (cnt[i] == max)
 				list.add(i + 1);
 		}
+		
 		int[] answer = new int[list.size()];
 		for (int i = 0; i < answer.length; i++) {
 			answer[i] = list.get(i);
