59e511dc350ca0f10f64170b76c21d566500e932
Han-kiyeon <kylin3100@naver.com>
2020-01-09 16:21:32 +0900

    190715 commit

---
+++ src/SWEA/D1/Solution_2046_스탬프찍기.java
@@ -0,0 +1,24 @@
+package SWEA.D1;
+
+import java.util.Scanner;
+
+/**
+ * @FileName : Solution_2046_스탬프찍기.java
+ * @Project : Algorithm_java
+ * @Date : 2019. 07. 15.
+ * @작성자 : 한기연
+ * @메모리 : 20,324 kb
+ * @실행시간 : 132 ms
+ **/
+public class Solution_2046_스탬프찍기 {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+
+		for (int tc = 1; tc <= T; tc++) {
+			System.out.print("#");
+
+		}
+	}
+}
