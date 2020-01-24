701875c62d01e45109f3fcd85e15ae6f8ff97b62
Han-kiyeon <kylin3100@naver.com>
2020-01-24 18:08:58 +0900

    BOJ_10430_나머지 commit

--- src/BOJ/Math/Bronze5_10430_나머지.java
+++ src/BOJ/Math/Bronze5_10430_나머지.java
@@ -1,6 +1,19 @@
+package BOJ.Math;
+
 import java.util.Scanner;
 
-public class Main {
+
+/**
+ * @Project : Algorithm_java
+ * @PackageName: BOJ.Math
+ * @FileName : Bronze5_10430_나머지.java
+ *
+ * @Date : 2020. 1. 24.
+ * @작성자 : 한기연
+ * @메모리 : __ kb
+ * @실행시간 : __ ms
+ **/
+public class Bronze5_10430_나머지 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
@@ -13,7 +26,7 @@ public class Main {
 		System.out.println((A % C + B % C) % C);
 		System.out.println((A * B) % C);
 		System.out.println((A % C * B % C) % C);
-		
+
 		sc.close();
 	}
 }
