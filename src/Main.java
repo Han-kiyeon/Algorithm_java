9cff57c14bbfc67a771e7a431ef4a537f4882711
Han-kiyeon <kylin3100@naver.com>
2020-01-29 09:36:38 +0900

    Naver 해커톤 문제

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,5 @@
-class Solution {
-	static int len, ans;
-	static boolean[] visit;
-	static String[] input;
-
-	public int solution(String[] A) {
-		input = A.clone();
-		len = A.length;
-
-		visit = new boolean[len];
-
-		ans = 0;
-		for (int i = 1; i <= len; i++) {
-			solve(i, 0, 0);
-		}
-
-		return ans;
-	}
-
-	private static void solve(int n, int idx, int depth) {
-		if (depth == n) {
-			check();
-			return;
-		}
-		for (int i = 0; i < len; i++) {
-			if (!visit[i]) {
-				visit[i] = true;
-				solve(n, i, depth + 1);
-				visit[i] = false;
-			}
-		}
-	}
-
-	private static void check() {
-		String str = "";
-		int[] alpabet = new int[26];
-		for (int i = 0; i < len; i++) {
-			if (visit[i])
-				str += input[i];
-		}
+class Main {
+	public static void main(String[] args) {
 		
-		for (int i = 0; i < str.length(); i++) {
-			alpabet[str.charAt(i) - 'a']++;
-		}
-
-		for (int i = 0; i < alpabet.length; i++) {
-			if (alpabet[i] > 1)
-				return;
-		}
-		if (ans < str.length())
-			ans = str.length();
 	}
 }
