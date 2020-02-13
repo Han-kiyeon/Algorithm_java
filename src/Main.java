efe0ad854b6a0c9debadaaf0ee6f54b03d0eb372
Han-kiyeon <kylin3100@naver.com>
2020-02-13 22:33:13 +0900

    리모컨 문제 해결

--- src/Main.java
+++ src/Main.java
@@ -1,84 +1,41 @@
-
+import java.io.FileInputStream;
 import java.util.Scanner;
 
 public class Main {
-	static int N, M, ans;
-	static int[][] map;
-	// 상 하 좌 우
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
+	static boolean[] broken;
 
 	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("res/input.txt"));
+		// System.setIn(new FileInputStream("res/input.txt"));
 		Scanner sc = new Scanner(System.in);
-		int TC = 0;
-		while (sc.hasNext()) {
-			N = sc.nextInt();
-			M = sc.nextInt();
-			map = new int[N][M];
-			int blank = -1; // 빈 칸의 개수
-			for (int i = 0; i < N; i++) {
-				String str = sc.next();
-				for (int j = 0; j < M; j++) {
-					if (str.charAt(j) == '*')
-						map[i][j] = 1;
-					else {
-						map[i][j] = 0;
-						blank++;
-					}
-				}
-			}
-
-			ans = 123456789;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if (map[i][j] == 0) {
-						map[i][j] = 1;
-						DFS(i, j, 0, blank);
-						map[i][j] = 0;
-					}
-				}
-			}
-			if (ans == 123456789)
-				ans = -1;
-			System.out.println("Case " + ++TC + ": " + ans);
-		}
-	}
-
-	private static void DFS(int i, int j, int cnt, int blank) {
-		if (blank == 0 && ans > cnt) {
-			ans = cnt;
+		int start = 100;
+		int goal = sc.nextInt();
+		if (start == goal) {
+			System.out.println(0);
 			return;
 		}
-
-		for (int dir = 0; dir < 4; dir++) {
-			int nr = i + dr[dir];
-			int nc = j + dc[dir];
-			if (isRange(nr, nc) && map[nr][nc] == 0) {
-				while (isRange(nr, nc) && map[nr][nc] == 0) {
-					map[nr][nc] = 1;
-					blank--;
-					nr += dr[dir];
-					nc += dc[dir];
-				}
-				nr -= dr[dir];
-				nc -= dc[dir];
-				DFS(nr, nc, cnt + 1, blank);
-
-				while (nr != i || nc != j) {
-					map[nr][nc] = 0;
-					blank++;
-					nr -= dr[dir];
-					nc -= dc[dir];
+		int N = sc.nextInt();
+		broken = new boolean[10];
+		for (int i = 0; i < N; i++) {
+			broken[sc.nextInt()] = true;
+		}
+		int ans = Math.abs(goal - start);
+		for (int i = 0; i <= 1000000; i++) {
+			if (go(i)) {
+				int tmp = Math.abs(i - goal) + ("" + i).length();
+				if (ans > tmp) {
+					ans = tmp;
 				}
 			}
-
 		}
+		System.out.println(ans);
 	}
 
-	private static boolean isRange(int nr, int nc) {
-		if (0 <= nr && nr < N && 0 <= nc && nc < M)
-			return true;
-		return false;
+	private static boolean go(int x) {
+		String str = "" + x;
+		for (int i = 0; i < str.length(); i++) {
+			if (broken[str.charAt(i) - '0'])
+				return false;
+		}
+		return true;
 	}
 }
