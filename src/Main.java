acba7eb245caafa5b09a0872975145d81abb6b95
Han-kiyeon <kylin3100@naver.com>
2020-02-18 18:12:31 +0900

    달이 차오른다 가자

--- src/Main.java
+++ src/Main.java
@@ -4,45 +4,93 @@ import java.util.Scanner;
 
 public class Main {
 	static int N, M, ans;
-	static boolean[][] graph;
-	static boolean[] visit;
+	static char[][] map;
+	static boolean[][][] visit;
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		N = sc.nextInt();
 		M = sc.nextInt();
-		graph = new boolean[N + 1][N + 1];
-		visit = new boolean[N + 1];
-		for (int i = 0; i < M; i++) {
-			int s = sc.nextInt();
-			int e = sc.nextInt();
-			graph[s][e] = true;
-			graph[e][s] = true;
-		}
-		ans = 0;
-		for (int i = 1; i <= N; i++) {
-			if (!visit[i]) {
-				BFS(i);
-				ans++;
-			}
-		}
-		System.out.println(ans);
-	}
+		map = new char[N][M];
+		visit = new boolean[1 << 6][N][M];
+		int si = 0, sj = 0;
 
-	private static void BFS(int x) {
-		Queue<Integer> q = new LinkedList<>();
-		q.offer(x);
-		visit[x] = true;
-		while (!q.isEmpty()) {
-			int cur = q.poll();
-			for (int i = 1; i <= N; i++) {
-				if (!visit[i] && graph[cur][i]) {
-					visit[i] = true;
-					q.offer(i);
+		for (int i = 0; i < N; i++) {
+			String str = sc.next();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = str.charAt(j);
+				if (str.charAt(j) == '0') {
+					si = i;
+					sj = j;
+					map[i][j] = '.';
 				}
 			}
 		}
 
+		ans = 1234567890;
+		BFS(si, sj);
+
+		if (ans == 1234567890)
+			System.out.println(-1);
+		else
+			System.out.println(ans);
+
+	}
+
+	private static void BFS(int y, int x) {
+		Queue<int[]> q = new LinkedList<>();
+		visit[0][y][x] = true;
+
+		q.offer(new int[] { 0, y, x, 0 });
+
+		while (!q.isEmpty()) {
+			int key = q.peek()[0];
+			int i = q.peek()[1];
+			int j = q.peek()[2];
+			int cnt = q.peek()[3];
+			q.poll();
+
+			if (map[i][j] == '1' && ans > cnt) {
+				ans = cnt;
+			}
+
+			for (int dir = 0; dir < 4; dir++) {
+				int r = i + dr[dir];
+				int c = j + dc[dir];
+				if (isRange(r, c) && !visit[key][r][c] && map[r][c] != '#') {
+					// 빈곳 || 출구
+					if (map[r][c] == '.' || map[r][c] == '1') {
+						visit[key][r][c] = true;
+						q.offer(new int[] { key, r, c, cnt + 1 }); // 이동
+					}
+					// 열쇠
+					else if ('a' <= map[r][c] && map[r][c] <= 'f' && !visit[key][r][c]) {
+						//System.out.println("get:" + map[r][c]);
+						int nkey = key | (1 << (map[r][c] - 'a')); // 키를 얻고
+						visit[key][r][c] = true;
+						q.offer(new int[] { nkey, r, c, cnt + 1 }); // 이동
+					}
+					// 문
+					else if ('A' <= map[r][c] && map[r][c] <= 'F' && !visit[key][r][c]) {
+						// 키가 있으면
+						if ((key & (1 << (map[r][c] - 'A'))) > 0) {
+							//System.out.println(" open:" + map[r][c]);
+							visit[key][r][c] = true;
+							q.offer(new int[] { key, r, c, cnt + 1 }); // 이동
+						}
+					}
+
+				}
+			}
+		}
+
+	}
+
+	private static boolean isRange(int r, int c) {
+		if (0 <= r && r < N && 0 <= c && c < M)
+			return true;
+		return false;
 	}
 }
