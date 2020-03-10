4ee8e71d9a8d403a8a86b512dba3cea53102e795
Han-kiyeon <kylin3100@naver.com>
2020-03-10 13:33:21 +0900

    200310 BOJ_1748_수이어쓰기1

--- src/BOJ/Math/Silver3_1748_수이어쓰기1.java
+++ src/BOJ/Math/Silver3_1748_수이어쓰기1.java
@@ -1,6 +1,19 @@
+package BOJ.Math;
 import java.util.Scanner;
 
-public class Main {
+/**
+ * @Project : Algorithm_java
+ * @PackageName: BOJ.Math
+ * @FileName : Silver3_1748_수이어쓰기1.java
+ *
+ * @Date : 2020. 3. 10.
+ * @작성자 : 한기연
+ * @메모리 : 14276 kb
+ * @실행시간 : 112 ms
+ *
+ * @Blog : __
+ **/
+public class Silver3_1748_수이어쓰기1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();// N(1≤N≤100,000,000)
