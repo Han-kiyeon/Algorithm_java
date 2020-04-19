8296cc01f768c1d5e8ad8fac83b82d611a25a1e3
Han-kiyeon <kylin3100@naver.com>
2020-04-19 16:07:06 +0900

    200419 BOJ_10159_저울

---
+++ src/BOJ/Graph/Gold3_10159_저울.java
@@ -0,0 +1,47 @@
+package BOJ.Graph;
+import java.util.Scanner;
+
+public class Gold3_10159_저울 {
+	static int N, M;
+	static boolean[][] graph;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+
+		graph = new boolean[N][N];
+
+		for (int i = 0; i < M; i++) {
+			int s = sc.nextInt() - 1;
+			int e = sc.nextInt() - 1;
+			graph[s][e] = true;
+		}
+
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				if (k == i)
+					continue;
+				for (int j = 0; j < N; j++) {
+					if (k == j || i == j || graph[i][j])
+						continue;
+					if (graph[i][k] && graph[k][j])
+						graph[i][j] = true;
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			int cnt = 0;
+			for (int j = 0; j < N; j++) {
+				if (i == j)
+					continue;
+				if (!graph[i][j] && !graph[j][i])
+					cnt++;
+			}
+			sb.append(cnt).append("\n");
+		}
+		System.out.println(sb);
+
+	}
+}
