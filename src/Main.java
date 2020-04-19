f0826e5263e3f8c7ff4745113352f3c674f0f40c
Han-kiyeon <kylin3100@naver.com>
2020-04-19 16:01:39 +0900

    플로이드 와샬

--- src/Main.java
+++ src/Main.java
@@ -1,85 +1,46 @@
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
-	static int N, K, L;
-	static int[][] map;
-
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
+	static int N, M;
+	static boolean[][] graph;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt(); // 보드의 크기 N
-		K = sc.nextInt(); // 사과의 개수 K
+		N = sc.nextInt();
+		M = sc.nextInt();
 
-		map = new int[N][N];
-		for (int i = 0; i < K; i++) {
-			map[sc.nextInt() - 1][sc.nextInt() - 1] = 1;
+		graph = new boolean[N][N];
+
+		for (int i = 0; i < M; i++) {
+			int s = sc.nextInt() - 1;
+			int e = sc.nextInt() - 1;
+			graph[s][e] = true;
 		}
 
-		L = sc.nextInt(); // 뱀의 방향 변환 횟수 L
-		LinkedList<int[]> cmd = new LinkedList<>();
-
-		for (int i = 0; i < L; i++) {
-			int t = sc.nextInt();
-			int d = sc.next().charAt(0) == 'L' ? -1 : 1;
-			cmd.add(new int[] { t, d });
-		}
-
-		int time = 0;
-		int dir = 1;
-		LinkedList<int[]> snake = new LinkedList<>();
-		snake.add(new int[] { 0, 0 });
-		while (true) {
-			time++;
-			int[] head = snake.getLast();
-
-			int[] tail = snake.getFirst();
-			int nr = head[0] + dr[dir];
-			int nc = head[1] + dc[dir];
-			System.out.println(time + ": " + Arrays.toString(head) + "size= " + snake.size() + " " + " >> next="+ nr + " " + nc+" :: " + map[nr][nc]);
-			
-			if (!isRange(nr, nc))
-				break;
-
-			if (map[nr][nc] == 0) {
-				snake.add(new int[] { nr, nc });
-				map[nr][nc] = 2;
-				map[tail[0]][tail[1]] = 0;
-				snake.remove(0);
-			} else if (map[nr][nc] == 1) {
-				System.out.println("사과");
-				snake.add(new int[] { nr, nc });
-				map[nr][nc] = 2;
-			} else
-				break;
-
-			if (!cmd.isEmpty() && time == cmd.getFirst()[0]) {
-
-				if (cmd.getFirst()[1] == 1) { // 오른쪽
-					if (dir == 3)
-						dir = 0;
-					else
-						dir++;
-				} else { // 왼쪽
-					if (dir == 0)
-						dir = 3;
-					else
-						dir--;
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				if (k == i)
+					continue;
+				for (int j = 0; j < N; j++) {
+					if (k == j || i == j || graph[i][j])
+						continue;
+					if (graph[i][k] && graph[k][j])
+						graph[i][j] = true;
 				}
-				cmd.remove(0);
-//				System.out.println("         time=" + time + ", dir=" + dir);
 			}
 		}
-		System.out.println(time);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			int cnt = 0;
+			for (int j = 0; j < N; j++) {
+				if (i == j)
+					continue;
+				if (!graph[i][j] && !graph[j][i])
+					cnt++;
+			}
+			sb.append(cnt).append("\n");
+		}
+		System.out.println(sb);
 
-	}
-
-	private static boolean isRange(int nr, int nc) {
-		if (0 <= nr && nr < N && 0 <= nc && nc < N)
-			return true;
-		return false;
 	}
 }
--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,19 @@
+import java.util.Arrays;
+
 public class Test {
 	public static void main(String[] args) {
-		int i = 0;
-		i |= 1;
-		i |= 2;
-		System.out.println(i);
+		int[][] map = new int[10][10];
 
-		System.out.println(i & 1 << 1);
+		int r = 3, c = 3;
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map.length; j++) {
+				if(Math.abs(i-r)+Math.abs(j-c)<=3)
+					map[i][j] = 1;
+			}
+		}
+
+		for (int[] is : map) {
+			System.out.println(Arrays.toString(is));
+		}
 	}
 }
