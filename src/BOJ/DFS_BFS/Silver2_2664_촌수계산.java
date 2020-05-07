26b6a2aea2fda02ada35474f7e93b11546614ec3
Han-kiyeon <kylin3100@naver.com>
2020-05-07 23:32:59 +0900

    200507 BOJ_2664_촌수계산

--- src/BOJ/DFS_BFS/Silver2_2664_촌수계산.java
+++ src/BOJ/DFS_BFS/Silver2_2664_촌수계산.java
@@ -1,8 +1,21 @@
+package BOJ.DFS_BFS;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main {
+/**
+ * @Project : Algorithm_java
+ * @PackageName: BOJ.DFS_BFS
+ * @FileName : Silver2_2664_촌수계산.java
+ *
+ * @Date : 2020. 5. 7.
+ * @작성자 : 한기연
+ * @메모리 : 14396 kb
+ * @실행시간 : 112 ms
+ *
+ * @Blog : __
+ **/
+public class Silver2_2664_촌수계산 {
 	static int N;
 	static boolean[][] graph;
 
