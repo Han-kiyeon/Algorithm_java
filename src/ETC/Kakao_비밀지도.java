47bc68375599fb9ec403048bf6ad0565a6d7d865
Han-kiyeon <kylin3100@naver.com>
2020-05-09 01:44:54 +0900

    200508 [2018 KAKAO BLIND RECRUITMENT] 비밀지도

--- src/ETC/Kakao_비밀지도.java
+++ src/ETC/Kakao_비밀지도.java
@@ -1,7 +1,8 @@
+package ETC;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Kakao_비밀지도 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = 5;
