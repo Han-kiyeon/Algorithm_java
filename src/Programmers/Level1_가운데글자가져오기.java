83f216d0a7b0ab46961658bf3338a2830941789c
Han-kiyeon <kylin3100@naver.com>
2020-05-17 02:36:23 +0900

    200517 [Programmers] 가운데 글자 가져오기

---
+++ src/Programmers/Level1_가운데글자가져오기.java
@@ -0,0 +1,15 @@
+package Programmers;
+
+public class Level1_가운데글자가져오기 {
+	public static void main(String[] args) {
+		System.out.println(solution("abcde"));// "c";
+		System.out.println(solution("qwer"));// "we";
+	}
+
+	public static String solution(String s) {
+		if (s.length() % 2 == 1)
+			return "" + s.charAt(s.length() / 2);
+		else
+			return "" + s.charAt(s.length() / 2 - 1) + s.charAt(s.length() / 2);
+	}
+}
