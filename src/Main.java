6293ab610c8ad0d2913a3fd616f27be89cd4f038
Han-kiyeon <kylin3100@naver.com>
2020-05-20 02:40:23 +0900

    주사위 윷놀이(feat.김도하)

--- src/Main.java
+++ src/Main.java
@@ -1,124 +1,182 @@
-import java.util.LinkedList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static final int WHITE = 0, RED = 1, BLUE = 2;
-	static final int[] change = { 1, 0, 3, 2 };
-	// →, ←, ↑, ↓
-	static final int[] dr = { 0, 0, -1, 1 };
-	static final int[] dc = { 1, -1, 0, 0 };
+	static int[] route0 = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40 };
+	static int[] route1 = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 28, 27, 26, 25, 30, 35,
+			40 };
+	static int[] route2 = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 25, 30, 35, 40 };
+	static int[] route3 = { 0, 2, 4, 6, 8, 10, 13, 16, 19, 25, 30, 35, 40 };
 
-	static int N, K;
-	static int[][] map;
-	static LinkedList<Integer>[][] state;
-	static Horse[] horses;
+	static int[] cmd;
+	static int[] move;
+	static int[][] horse;
 
-	static class Horse {
-		int r, c, dir;
-
-		Horse(int r, int c, int dir) {
-			this.r = r;
-			this.c = c;
-			this.dir = dir;
-		}
-	}
+	static int max;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt(); // 체스판의 크기 N
-		K = sc.nextInt(); // 말의 개수 K
 
-		map = new int[N][N]; // 체스판 색상 정보
-		state = new LinkedList[N][N]; // 체스판에 쌓인 말의 순서
+		cmd = new int[10];
+		for (int i = 0; i < 10; i++) { // 입력되는 명령어의 수 10개
+			cmd[i] = sc.nextInt();
+		}
+		sc.close();
+		// 입력 끝
 
-		// 체스판의 정보
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				map[i][j] = sc.nextInt();
-				state[i][j] = new LinkedList<>();
+		move = new int[10];
+		max = Integer.MIN_VALUE;
+		DFS(0);
+		// int[] tmp = { 0, 0, 1, 1, 0, 0, 0, 0, 1, 1 };
+		// move = tmp.clone();
+		// System.out.println(solve());
+		System.out.println(max);
+	}
+
+	private static void DFS(int depth) {
+		if (depth == cmd.length) {
+			int sum = solve();
+			if (max < sum) {
+				max = sum;
+				// System.out.println(sum);
+				// System.out.println(Arrays.toString(move));
+				// for (int[] is : horse) {
+				// System.out.println(Arrays.toString(is));
+				// }
 			}
+			return;
 		}
 
-		// 말의 정보
-		horses = new Horse[K + 1]; // 1 ~ K번 말
-		for (int i = 1; i <= K; i++) {
-			int r = sc.nextInt() - 1;
-			int c = sc.nextInt() - 1;
-			int dir = sc.nextInt() - 1;
-			horses[i] = new Horse(r, c, dir);
-			state[r][c].add(i);
+		for (int i = 0; i < 4; i++) {
+			move[depth] = i;
+			DFS(depth + 1);
 		}
-
-		System.out.println(start());
 
 	}
 
-	private static int start() {
-		boolean flag = true;
-		int times = 0;
-		while (flag) {
-			times++;
-			if (times > 1000)
-				break;
+	private static int solve() {
+		horse = new int[4][2]; // 말의 위치, 가는 길, 움직인 횟수
+		int sum = 0;
 
-			for (int i = 1; i <= K; i++) {
-				Horse h = horses[i];
-				int r = h.r;
-				int c = h.c;
+		for (int i = 0; i < 10; i++) {
+			int cur = move[i]; // 현재움직일 말의 번호.
+			horse[cur][0] += cmd[i]; // 움직였을때의 위치.
+			int tmp = horse[cur][1];
+			// System.out.println("i=" + i + ", cur=" + cur + ", cmd[i]=" + cmd[i]);
+			// System.out.println("현재 말: " + cur);
+			// System.out.println("움직인 위치: " + horse[cur][0]);
+			// System.out.println("가고있는길" + horse[cur][1]);
 
-				// 가장 아래쪽 말이 아니라면 PASS
-				if (state[r][c].get(0) != i)
-					continue;
+			// ================길 변경================
+			if (horse[cur][1] == 0) {
+				if (horse[cur][0] == 5)
+					horse[cur][1] = 3;
+				else if (horse[cur][0] == 10)
+					horse[cur][1] = 2;
+				else if (horse[cur][0] == 15)
+					horse[cur][1] = 1;
 
-				int nr = r + dr[h.dir];
-				int nc = c + dc[h.dir];
+				// System.out.println("change" + horse[cur][1]);
+			}
 
-				// 말이 이동하려는 칸이 파란색인 경우 + 체스판을 벗어나는 경우
-				if (!isRange(nr, nc) || map[nr][nc] == BLUE) {
-					// 방향 반대로
-					h.dir = change[h.dir];
-					nr = r + dr[h.dir];
-					nc = c + dc[h.dir];
-				}
-
-				// 방향을 반대로 한 후에 이동하려는 칸이 파란색인 경우
-				if (!isRange(nr, nc) || map[nr][nc] == BLUE) {
-					continue;
-				}
-				// 말이 이동하려는 칸이 빨간색인 경우
-				else if (map[nr][nc] == RED) {
-					// 순서를 반대로 모든 말이 이동
-					for (int j = state[r][c].size() - 1; j >= 0; j--) {
-						int tmp = state[r][c].get(j);
-						state[nr][nc].add(tmp);
-						horses[tmp].r = nr;
-						horses[tmp].c = nc;
+			// ================중복체크================
+			// 이동하려고 하는 칸에 말이 이미 있는 경우에는 그 칸으로 이동할 수 없다.
+			// --------------1--2--3--4---5---6---7---8---9--10--11--12--13--14--15--16--17--18--19--20--21--22
+			// route0 = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40 };
+			// route1 = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 28, 27, 26, 25, 30, 35, 40 };
+			// route2 = { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 25, 30, 35, 40 };
+			// route3 = { 0, 2, 4, 6, 8, 10, 13, 16, 19, 25, 30, 35, 40 };
+			boolean flag = false;
+			int j = 0;
+			for (; j < horse.length; j++) {
+				if (cur != j) {
+					if (horse[cur][0] == horse[j][0] && horse[cur][1] == horse[j][1]) {
+						flag = true;
+						break;
+					} else if ((horse[cur][1] == 1 && horse[cur][0] == 19)
+							|| (horse[cur][1] == 2 && horse[cur][0] == 13)
+							|| (horse[cur][1] == 3 && horse[cur][0] == 9)) {
+						// 25번에 도착했을때
+						if (horse[j][1] == 1 && horse[j][0] == 19) {// 1번길에 같은위치
+							flag = true;
+							break;
+						} else if (horse[j][1] == 2 && horse[j][0] == 13) {// 2번길에 같은위치
+							flag = true;
+							break;
+						} else if (horse[j][1] == 3 && horse[j][0] == 9) {// 3번길에 같은위치
+							flag = true;
+							break;
+						}
+					} else if ((horse[cur][1] == 1 && horse[cur][0] == 20)
+							|| (horse[cur][1] == 2 && horse[cur][0] == 14)
+							|| (horse[cur][1] == 3 && horse[cur][0] == 10)) {
+						// 30번에 도착했을때
+						if (horse[j][1] == 1 && horse[j][0] == 20) {// 1번길에 같은위치
+							flag = true;
+							break;
+						} else if (horse[j][1] == 2 && horse[j][0] == 14) {// 2번길에 같은위치
+							flag = true;
+							break;
+						} else if (horse[j][1] == 3 && horse[j][0] == 10) {// 3번길에 같은위치
+							flag = true;
+							break;
+						}
 					}
-					state[r][c].clear();
-				}
-				// 말이 이동하려는 칸이 흰색인 경우
-				else {
-					// 모든 말이 이동
-					for (int j = 0; j < state[r][c].size(); j++) {
-						int tmp = state[r][c].get(j);
-						state[nr][nc].add(tmp);
-						horses[tmp].r = nr;
-						horses[tmp].c = nc;
-					}
-					state[r][c].clear();
-				}
 
-				// 이동한 곳에 말이 4개 이상 있는가?
-				if (state[nr][nc].size() >= 4) {
-					flag = false;
-					break;
+					else if ((horse[cur][1] == 1 && horse[cur][0] == 21)
+							|| (horse[cur][1] == 2 && horse[cur][0] == 15)
+							|| (horse[cur][1] == 3 && horse[cur][0] == 11)) {
+						// 35번에 도착했을때
+						if (horse[j][1] == 1 && horse[j][0] == 21) {// 1번길에 같은위치
+							flag = true;
+							break;
+						} else if (horse[j][1] == 2 && horse[j][0] == 15) {// 2번길에 같은위치
+							flag = true;
+							break;
+						} else if (horse[j][1] == 3 && horse[j][0] == 11) {// 3번길에 같은위치
+							flag = true;
+							break;
+						}
+					} else if ((horse[cur][1] == 0 && horse[cur][0] == 20)
+							|| (horse[cur][1] == 1 && horse[cur][0] == 22)
+							|| (horse[cur][1] == 2 && horse[cur][0] == 16)
+							|| (horse[cur][1] == 3 && horse[cur][0] == 12)) {
+						// 40번에 도착했을때
+						if (horse[j][1] == 0 && horse[j][0] == 20) {// 0번길에 같은위치
+							flag = true;
+							break;
+						} else if (horse[j][1] == 1 && horse[j][0] == 22) {// 1번길에 같은위치
+							flag = true;
+							break;
+						} else if (horse[j][1] == 2 && horse[j][0] == 16) {// 2번길에 같은위치
+							flag = true;
+							break;
+						} else if (horse[j][1] == 3 && horse[j][0] == 12) {// 3번길에 같은위치
+							flag = true;
+							break;
+						}
+					}
 				}
 			}
+			if (flag) {
+				return Integer.MIN_VALUE;
+			}
+
+			// ================더하기================
+			if (horse[cur][1] == 0 && horse[cur][0] < route0.length)
+				sum += route0[horse[cur][0]];
+			else if (horse[cur][1] == 1 && horse[cur][0] < route1.length)
+				sum += route1[horse[cur][0]];
+			else if (horse[cur][1] == 2 && horse[cur][0] < route2.length)
+				sum += route2[horse[cur][0]];
+			else if (horse[cur][1] == 3 && horse[cur][0] < route3.length)
+				sum += route3[horse[cur][0]];
+
+			// System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+sum);
+
 		}
-		return flag ? -1 : times;
+
+		return sum;
 	}
 
-	static boolean isRange(int r, int c) {
-		return 0 <= r && r < N && 0 <= c && c < N;
-	}
 }
---
+++ src/boj17825.java
@@ -0,0 +1,106 @@
+import java.util.*;
+import java.io.*;
+
+public class boj17825 {
+	static int answer;
+	static int data[] = new int[10];
+	static int list[] = new int[10];
+	static int map[][] = {
+			{ 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40 },
+			{ 10, 13, 16, 19, 25, 30, 35, 40 },
+			{ 20, 22, 24, 25, 30, 35, 40 },
+			{ 30, 28, 27, 26, 25, 30, 35, 40 },
+			{ 25, 30, 35, 40 }
+	};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < 10; i++)
+			data[i] = Integer.parseInt(st.nextToken());
+
+		answer = 0;
+		go(0);
+		System.out.println(answer);
+	}
+
+	static void go(int v) {
+		if (v == 10) {
+			int ret = Test();
+			if (answer < ret)
+				answer = ret;
+			return;
+		}
+
+		for (int i = 0; i < 4; i++) {
+			list[v] = i;
+			go(v + 1);
+		}
+	}
+
+	static int Test() {
+		int ret = 0;
+		// 4개의 말이 있다.
+		int horse[][] = new int[4][2]; // 맵번호와 위치를 저장
+		for (int i = 0; i < 10; i++) {
+			int cur = list[i]; // 옮길 말의 번호
+			int move = data[i];// 얼마나 움직일지
+
+			// 이미 도착한 말을 이동하려하면 종료
+			if (horse[cur][1] == -1)
+				return 0;
+
+			// 옮긴 위치가 도착점이 아닐 때(맵을 벗어난 경우는 도착을 의미)
+			if (horse[cur][1] + move < map[horse[cur][0]].length) {
+				// 이동
+				horse[cur][1] += move;
+
+				// 맵 분기
+				int idx = horse[cur][0]; // 맵 번호
+				int pos = horse[cur][1]; // 위치
+				if (idx == 0) { // 맵 번호가 0일때 분기 가능한 위치들
+					if (map[0][pos] == 10) {
+						horse[cur][0] = 1;
+						horse[cur][1] = 0;
+					} else if (map[0][pos] == 20) {
+						horse[cur][0] = 2;
+						horse[cur][1] = 0;
+					} else if (map[0][pos] == 30) {
+						horse[cur][0] = 3;
+						horse[cur][1] = 0;
+					} else if (map[0][pos] == 40) {
+						horse[cur][0] = 4;
+						horse[cur][1] = 3;
+					}
+				} else if (idx != 4) { // 맵 번호가 1, 2, 3번일 때
+					int[] common = { 25, 30, 35, 40 };
+					for (int j = 0; j < common.length; j++) {
+						if (map[idx][pos] == common[j]) {
+							horse[cur][0] = 4;
+							horse[cur][1] = j;
+						}
+					}
+				}
+
+				// 다른 말이 있는지 검사하기.
+				for (int j = 0; j < 4; j++) {
+					int[] other = horse[j];
+					// 같은 말, 도착, 출발 안함.
+					if (cur == j || other[1] == -1 || map[other[0]][other[1]] == 0)
+						continue;
+					// 옮긴 위치가 다른 말의 위치랑 같을 때
+					if (horse[cur][0] == other[0] && horse[cur][1] == other[1])
+						return 0;
+				}
+
+				// 이상 없다면 점수 누적
+				ret += map[horse[cur][0]][horse[cur][1]];
+			}
+			// 도착
+			else
+				horse[cur][1] = -1;
+		}
+		return ret;
+	}
+}
