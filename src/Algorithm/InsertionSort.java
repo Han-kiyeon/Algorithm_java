e407a3d1032dc6f56d6e6be89367d9bd8b2f20ec
Han-kiyeon <kylin3100@naver.com>
2020-07-12 14:24:12 +0900

    2019-07-12 Insertion Sort

--- src/Algorithm/InsertionSort.java
+++ src/Algorithm/InsertionSort.java
@@ -1,16 +1,26 @@
 package Algorithm;
 
 import java.util.Arrays;
+import java.util.Random;
 
-public class InsertSort {
+public class InsertionSort {
+
+	static final int N = 10;
+
 	public static void main(String[] args) {
-		int[] arr = { 4, 5, 7, 3, 9, 0 };
+		Random random = new Random(); // 랜덤함수를 이용
+
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = random.nextInt(100); // 0 ~ 99
+		}
+
 		System.out.println("정렬 전: " + Arrays.toString(arr));
-		insertSort(arr);
+		insertionSort(arr);
 		System.out.println("정렬 후: " + Arrays.toString(arr));
 	}
 
-	private static void insertSort(int[] arr) {
+	private static void insertionSort(int[] arr) {
 		for (int i = 1; i < arr.length; i++) {
 			int temp = arr[i];
 			int j = i - 1;
